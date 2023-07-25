95574fc9a2bd40a6b1962c69b975046f720db212
Michael Pavlov <micha.pavlov@gmail.com>
2023-07-25 12:02:46 +0200

    Add graphics & animation to the checkpoint

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,10 +1,34 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [Header("Animation")]
+    [SerializeField] 
+    private Vector3 loopOffset;
+    [SerializeField] 
+    private float loopDuration;
+    [SerializeField] 
+    private float fadeDuration;
+    [SerializeField] 
+    private Vector3 fadeOffset;
+
+    private Tween loopAnimation;
+
+    [Header("Utility")]
     [SerializeField] 
     private GameObject checkpointManager;
-    
+
+    void Start()
+    {
+        var currentPosition = transform.position;
+        Vector3[] waypoints = {currentPosition, currentPosition + loopOffset, currentPosition};
+
+        loopAnimation = transform.DOPath(waypoints, loopDuration, PathType.Linear, PathMode.Sidescroller2D)
+            .SetLoops(-1)
+            .SetEase(Ease.Linear);
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag("Player")) return;
@@ -19,6 +43,8 @@ public class Checkpoint : MonoBehaviour
             CheckpointManager.Instance.SetPosition();   
         }
 
-        //TODO: Animate the checkpoint
+        loopAnimation.Pause();
+        transform.DOMove(transform.position - fadeOffset, fadeDuration);
+        GetComponent<SpriteRenderer>().DOColor(Color.clear, fadeDuration);
     }
 }
